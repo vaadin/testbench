2e980833ef71924e02a42c15159c0192026ca4f9
artur.signell@itmill.com <artur.signell@itmill.com@de684a11-831f-0410-ab27-cb86445eb682>
2009-03-27 07:29:39 +0000

    Initial import of Selenium extensions
    git-svn-id: http://dev.vaadin.com/svn/TestingTools/2.0@7204 de684a11-831f-0410-ab27-cb86445eb682

---
+++ selenium-extensions/ide-extensions.js
@@ -0,0 +1,40 @@
+
+LocatorBuilders.add('itmilltoolkit', function(e) {
+	var wnd = this.window;
+	if (wnd.wrappedJSObject) {
+		wnd = wnd.wrappedJSObject;
+	}
+	
+	if (!wnd.itmill) {
+		// Not a toolkit application
+		return null;
+	}
+	
+	// Unwrap the element if wrapped so we can access tkPid
+	if (e.wrappedJSObject) {
+		e = e.wrappedJSObject;
+	}
+	
+	
+	for(var windowname in wnd.itmill.clients) {
+		var path = wnd.itmill.clients[windowname].getPathForElement(e);
+		if (path != null) {
+			return "itmilltoolkit=" + windowname + "::" + path;
+		}
+	}
+	
+	return null;
+});
+
+// Add itmilltoolkit as the first locator
+{
+	var order = ["itmilltoolkit"];
+	for (var i=0; i < LocatorBuilders.order.length; i++) {
+		if (LocatorBuilders.order[i] != "itmilltoolkit") {
+			order.push(LocatorBuilders.order[i]);
+		}
+	}
+	
+	LocatorBuilders.order = order;
+}
+
