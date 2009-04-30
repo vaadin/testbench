ea2bae688a4347e2bf4e39e80598b5ad1c333ccf
artur.signell@itmill.com <artur.signell@itmill.com@de684a11-831f-0410-ab27-cb86445eb682>
2009-04-30 08:37:41 +0000

    Changed TestConverter to create a single file with all tests for each browser
    git-svn-id: http://dev.vaadin.com/svn/TestingTools/2.0@7585 de684a11-831f-0410-ab27-cb86445eb682

--- src/com/vaadin/testingtools/util/TestConverter.java
+++ src/com/vaadin/testingtools/util/TestConverter.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -41,10 +42,12 @@ public class TestConverter {
             + "import com.vaadin.testingtools.testcase.AbstractVaadinTestCase;\n"
             + "\n" + "public class {class} extends AbstractVaadinTestCase {\n"
             + "\n" + "public void setUp() throws Exception {\n"
-            + "        setBrowser({browser});\n super.setUp();\n" + "}" + "\n"
-            + "\n" + "public void test{testName}() throws Exception {\n" + "\n";
+            + "        setBrowser({browser});\n super.setUp();\n" + "}" + "\n";
 
-    private static final String JAVA_FOOTER = "}\n" + "}\n";
+    private static final String TEST_METHOD_HEADER = "public void test{testName}() throws Exception {\n";
+    private static final String TEST_METHOD_FOOTER = "}\n";
+
+    private static final String JAVA_FOOTER = "}\n";
 
     private static final String PACKAGE_DIR = "vaadin/automatedtests";
 
@@ -66,20 +69,78 @@ public class TestConverter {
         for (String browser : browsers) {
             System.out.println("Generating tests for " + browser);
 
-            String browserId = knownBrowsers.get(browser.toLowerCase());
-            if (browserId == null) {
-                System.err.println("Warning: Unknown browser: " + browser);
-            }
+            OutputStream out = null;
+            try {
+                String browserId = knownBrowsers.get(browser.toLowerCase());
+                if (browserId == null) {
+                    System.err.println("Warning: Unknown browser: " + browser);
+                }
 
-            for (int i = 2; i < args.length; i++) {
-                String filename = args[i];
+                // Create a java file for holding all tests for this browser
+                out = createJavaFileForBrowser(browser, outputDirectory);
+
+                // Write the tests to the java file
+                for (int i = 2; i < args.length; i++) {
+                    String filename = args[i];
+                    try {
+                        String testMethod = createTestMethod(filename,
+                                getTestName(filename));
+                        out.write(testMethod.getBytes());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                // Write the footer to the browser test class.
+                writeJavaFooter(out);
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } finally {
                 try {
-                    convertFile(filename, browser, outputDirectory);
+                    if (out != null) {
+                        out.flush();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }
+    }
+
+    private static void writeJavaFooter(OutputStream out) throws IOException {
+        String footer = getJavaFooter();
+        out.write(footer.getBytes());
+
+    }
+
+    private static String getTestName(String filename) {
+        File f = new File(filename);
+        return removeExtension(f.getName());
+    }
+
+    private static OutputStream createJavaFileForBrowser(String browser,
+            String outputDirectory) throws IOException {
+        File outputFile = getJavaFile(browser, outputDirectory);
+        System.out.println("Creating " + outputFile + " for " + browser
+                + " tests");
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+
+        outputStream.write(getJavaHeader(getSafeName(browser), browser));
+
+        return outputStream;
+    }
+
+    private static String getSafeName(String name) {
+        return name.replaceAll("[^a-zA-Z0-9]", "_");
     }
 
     private static void createIfNotExists(String directory) {
@@ -95,13 +156,8 @@ public class TestConverter {
 
     }
 
-    private static void convertFile(String htmlFile, String browser,
-            String outputDirectory) throws FileNotFoundException, IOException {
-        File outputFile = getJavaFile(htmlFile, browser, outputDirectory);
-        System.out.println("Converting " + htmlFile + " into "
-                + outputFile.getAbsolutePath());
-        String className = removeExtension(outputFile.getName());
-        // outputFile.createNewFile();
+    private static String createTestMethod(String htmlFile, String testName)
+            throws FileNotFoundException, IOException {
 
         String htmlSource = IOUtils.toString(new FileInputStream(htmlFile));
         htmlSource = htmlSource.replace("\"", "\\\"")
@@ -112,31 +168,45 @@ public class TestConverter {
             Scriptable scope = cx.initStandardObjects();
 
             List<Command> commands = parseTestCase(cx, scope, htmlSource);
-            String javaFile = convertTestCaseToJava(commands);
-
-            FileOutputStream outputStream = new FileOutputStream(outputFile);
-
-            outputStream.write(getJavaHeader(className, browser));
-            outputStream.write(javaFile.getBytes());
-            outputStream.write(getJavaFooter());
-
-            outputStream.close();
-
-            System.out.println("Done");
+            String testCaseMethod = createTestCaseMethod(testName, commands);
+            return testCaseMethod;
+            // System.out.println("Done");
         } finally {
             Context.exit();
         }
 
     }
 
+    private static String createTestCaseMethod(String testName,
+            List<Command> commands) {
+        String testCaseHeader = getTestCaseHeader(testName);
+        String testCaseBody = convertTestCaseToJava(commands);
+        String testCaseFooter = getTestCaseFooter(testName);
+
+        return testCaseHeader + testCaseBody + testCaseFooter;
+    }
+
     private static String removeExtension(String name) {
         return name.replaceAll("\\.[^\\.]*$", "");
+    }
+
+    private static String getTestCaseHeader(String testName) {
+        String header = TEST_METHOD_HEADER;
+        header = header.replace("{testName}", testName);
+
+        return header;
+    }
+
+    private static String getTestCaseFooter(String testName) {
+        String footer = TEST_METHOD_FOOTER;
+        footer = footer.replace("{testName}", testName);
+
+        return footer;
     }
 
     private static byte[] getJavaHeader(String className, String browser) {
         String header = JAVA_HEADER;
         header = header.replace("{class}", className);
-        header = header.replace("{testName}", className);
         header = header.replace("{package}", getPackageName());
 
         String browserId = knownBrowsers.get(browser.toLowerCase());
@@ -149,17 +219,15 @@ public class TestConverter {
         return header.getBytes();
     }
 
-    private static byte[] getJavaFooter() {
-        return JAVA_FOOTER.getBytes();
+    private static String getJavaFooter() {
+        return JAVA_FOOTER;
     }
 
-    private static File getJavaFile(String htmlFilename, String browser,
-            String outputDirectory) {
+    private static File getJavaFile(String browser, String outputDirectory) {
+        String filenameSafeBrowser = getSafeName(browser);
 
-        File file = new File(htmlFilename);
+        File file = new File(filenameSafeBrowser);
         String filename = removeExtension(file.getName());
-        filename = filename.replaceAll("[^a-zA-Z0-9]", "_");
-        filename += "_" + browser.replaceAll("[^a-zA-Z0-9]", "_");
 
         File outputFile = new File(outputDirectory + File.separator
                 + getPackageDir() + File.separator + filename + ".java");
@@ -167,8 +235,7 @@ public class TestConverter {
         return outputFile;
     }
 
-    private static String convertTestCaseToJava(List<Command> commands)
-            throws IOException {
+    private static String convertTestCaseToJava(List<Command> commands) {
         StringBuilder javaSource = new StringBuilder();
 
         for (Command command : commands) {
