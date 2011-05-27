addc2ede839ba3579c7e0319199d1b9ecc2c52ac
matti.tahvonen@itmill.com <matti.tahvonen@itmill.com@de684a11-831f-0410-ab27-cb86445eb682>
2011-05-27 09:43:50 +0000

    added test converter that searches given directory hierarchically and converts all tests from it
    git-svn-id: http://dev.vaadin.com/svn/TestingTools/2.0@19114 de684a11-831f-0410-ab27-cb86445eb682

---
+++ src/com/vaadin/testbench/util/DirectoryTestConverter.java
@@ -0,0 +1,37 @@
+package com.vaadin.testbench.util;
+
+import java.io.File;
+
+/**
+ * Does the same as TestConverter, but accepts a folder as a root from which
+ * tests are found. Somewhat more usable than listing html files separately.
+ */
+public class DirectoryTestConverter {
+
+    public static void main(String[] args) throws Exception {
+        String dir = args[2];
+        File file = new File(dir);
+        File[] listFiles = file.listFiles();
+        String[] fakeArgs = args.clone();
+        convertTests(listFiles, fakeArgs);
+
+    }
+
+    private static void convertTests(File[] listFiles,
+            String[] fakeArgs) throws Exception {
+        for (File file2 : listFiles) {
+            if(file2.isDirectory()) {
+                convertTests(file2.listFiles(), fakeArgs);
+            } else if (file2.getName().endsWith(".html")) {
+                String path = file2.getPath();
+                fakeArgs[fakeArgs.length -1] = path;
+                try {
+                	TestConverter.main(fakeArgs);
+                } catch (Exception e) {
+					// TODO: handle exception
+				}
+            }
+        }
+    }
+
+}
