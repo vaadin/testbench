6aba799202bd7f2127a01a21f7c59173f77370d5
Jonatan Kronqvist <jonatan@oxygenite.org>
2012-01-24 13:09:05 +0200

    Added dummy test to test that selenium 2 tests work

---
+++ test/src/com/vaadin/testbench/tests/TestSeleniumWorking.java
@@ -0,0 +1,29 @@
+package com.vaadin.testbench.tests;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestSeleniumWorking {
+    private FirefoxDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new FirefoxDriver();
+    }
+
+    @After
+    public void tareDown() {
+        driver.close();
+    }
+
+    @Test
+    public void testScreenshot() throws IOException {
+        driver.navigate().to("http://google.com");
+        // File screenshot = driver.getScreenshotAs(OutputType.FILE);
+        // FileUtils.copyFile(screenshot, new File("/tmp/shot.png"));
+    }
+}
