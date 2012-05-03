e383ff46cf74e15090e21a900b6165fd27652d3e
Jonatan Kronqvist <jonatan@vaadin.com>
2012-05-03 13:20:07 +0300

    TestBenchElement implements WebElement

--- src/com/vaadin/testbench/TestBenchElement.java
+++ src/com/vaadin/testbench/TestBenchElement.java
@@ -1,12 +1,18 @@
 package com.vaadin.testbench;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.internal.WrapsElement;
 
 import com.vaadin.testbench.commands.CanWaitForVaadin;
 import com.vaadin.testbench.commands.TestBenchElementCommands;
 
-public class TestBenchElement implements WrapsElement,
+public class TestBenchElement implements WrapsElement, WebElement,
         TestBenchElementCommands, CanWaitForVaadin {
     // private static final Logger LOGGER = Logger
     // .getLogger(TestBenchElement.class.getName());
@@ -32,6 +38,86 @@ public class TestBenchElement implements WrapsElement,
     @Override
     public void waitForVaadin() {
         tbDriver.waitForVaadin();
+    }
+
+    @Override
+    public void click() {
+        actualElement.click();
+    }
+
+    @Override
+    public void submit() {
+        actualElement.click();
+    }
+
+    @Override
+    public void sendKeys(CharSequence... keysToSend) {
+        actualElement.sendKeys(keysToSend);
+    }
+
+    @Override
+    public void clear() {
+        actualElement.clear();
+    }
+
+    @Override
+    public String getTagName() {
+        return actualElement.getTagName();
+    }
+
+    @Override
+    public String getAttribute(String name) {
+        return actualElement.getAttribute(name);
+    }
+
+    @Override
+    public boolean isSelected() {
+        return actualElement.isSelected();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return actualElement.isEnabled();
+    }
+
+    @Override
+    public String getText() {
+        return actualElement.getText();
+    }
+
+    @Override
+    public List<WebElement> findElements(By by) {
+        List<WebElement> elements = actualElement.findElements(by);
+        List<WebElement> tbElements = new ArrayList<WebElement>(elements.size());
+        for (WebElement e : elements) {
+            tbElements.add(TestBench.createElement(e, tbDriver));
+        }
+        return tbElements;
+    }
+
+    @Override
+    public WebElement findElement(By by) {
+        return TestBench.createElement(actualElement.findElement(by), tbDriver);
+    }
+
+    @Override
+    public boolean isDisplayed() {
+        return actualElement.isDisplayed();
+    }
+
+    @Override
+    public Point getLocation() {
+        return actualElement.getLocation();
+    }
+
+    @Override
+    public Dimension getSize() {
+        return actualElement.getSize();
+    }
+
+    @Override
+    public String getCssValue(String propertyName) {
+        return actualElement.getCssValue(propertyName);
     }
 
 }
