4faf92bdc86f9cfa70ab3b30e4b8492f65e4e120
Jonatan Kronqvist <jonatan@vaadin.com>
2012-05-09 10:38:12 +0300

    Added some JavaDoc

--- src/com/vaadin/testbench/TestBenchTestCase.java
+++ src/com/vaadin/testbench/TestBenchTestCase.java
@@ -6,6 +6,14 @@ import com.vaadin.testbench.commands.TestBenchCommands;
 
 public class TestBenchTestCase {
 
+    /**
+     * Convenience method that casts the specified {@link WebDriver} instance to
+     * an instance of {@link TestBenchCommands}, making it easy to access the
+     * special TestBench commands.
+     * 
+     * @param webDriver
+     * @return
+     */
     public TestBenchCommands testBench(WebDriver webDriver) {
         return (TestBenchCommands) webDriver;
     }
