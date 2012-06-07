affa8154884649a80d4d59e24cac75c46798121c
Jonatan Kronqvist <jonatan@vaadin.com>
2012-06-07 15:26:46 +0300

    Recorder themeing: finishing touches - still need to get the first test case highlighted on startup. currently the style doesn't get applied before mouseover

--- testbench-recorder/main/src/content/suiteTreeView.js
+++ testbench-recorder/main/src/content/suiteTreeView.js
@@ -175,6 +175,9 @@ objectExtend(SuiteTreeView.prototype, {
             return text;
         },
         getRowProperties: function(row, props) {
+            if (row == this.currentTestCaseIndex) {
+                props.AppendElement(XulUtils.atomService.getAtom("currentTestCase"));
+            }
             if (this.selection.isSelected(row)) return;
             var testCase = this.getTestSuite().tests[row];
             if (testCase.testResult) {
