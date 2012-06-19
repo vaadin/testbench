40134b195f4f36ac487b9461526e555478161353
Jonatan Kronqvist <jonatan@vaadin.com>
2012-06-19 10:04:47 +0300

    Fixed the tests for the reference name generator

--- test/src/com/vaadin/testbench/screenshot/ReferenceNameGeneratorTest.java
+++ test/src/com/vaadin/testbench/screenshot/ReferenceNameGeneratorTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.Platform;
 
 public class ReferenceNameGeneratorTest {
 
@@ -22,11 +23,12 @@ public class ReferenceNameGeneratorTest {
     public void testGenerateName_shotFirefox11inCapabilities_returnsGeneratedName() {
         ReferenceNameGenerator rng = new ReferenceNameGenerator();
         Capabilities ffcaps = createNiceMock(Capabilities.class);
+        expect(ffcaps.getPlatform()).andReturn(Platform.XP);
         expect(ffcaps.getBrowserName()).andReturn("Firefox");
         expect(ffcaps.getVersion()).andReturn("11");
         replay(ffcaps);
         String name = rng.generateName("shot", ffcaps);
-        assertEquals("shot_Firefox_11", name);
+        assertEquals("shot_XP_Firefox_11", name);
         verify(ffcaps);
     }
 
@@ -34,23 +36,25 @@ public class ReferenceNameGeneratorTest {
     public void testGenerateName_shotChrome14inCapabilities_returnsGeneratedName() {
         ReferenceNameGenerator rng = new ReferenceNameGenerator();
         Capabilities chrome = createNiceMock(Capabilities.class);
+        expect(chrome.getPlatform()).andReturn(Platform.LINUX);
         expect(chrome.getBrowserName()).andReturn("Chrome");
         expect(chrome.getVersion()).andReturn("14");
         replay(chrome);
         String name = rng.generateName("shot", chrome);
-        assertEquals("shot_Chrome_14", name);
+        assertEquals("shot_LINUX_Chrome_14", name);
         verify(chrome);
     }
 
     @Test
     public void testGenerateName_fooSafari5inCapabilities_returnsGeneratedName() {
         ReferenceNameGenerator rng = new ReferenceNameGenerator();
-        Capabilities chrome = createNiceMock(Capabilities.class);
-        expect(chrome.getBrowserName()).andReturn("Safari");
-        expect(chrome.getVersion()).andReturn("5");
-        replay(chrome);
-        String name = rng.generateName("foo", chrome);
-        assertEquals("foo_Safari_5", name);
-        verify(chrome);
+        Capabilities safari = createNiceMock(Capabilities.class);
+        expect(safari.getPlatform()).andReturn(Platform.MAC);
+        expect(safari.getBrowserName()).andReturn("Safari");
+        expect(safari.getVersion()).andReturn("5");
+        replay(safari);
+        String name = rng.generateName("foo", safari);
+        assertEquals("foo_MAC_Safari_5", name);
+        verify(safari);
     }
 }
