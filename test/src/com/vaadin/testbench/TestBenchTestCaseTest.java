2800de1d9a0f8c4b5290497385f58a89e17ede00
Jonatan Kronqvist <jonatan@vaadin.com>
2013-02-14 13:09:12 +0200

    Refactored the isElementPresent test case a bit #10815

--- test/src/com/vaadin/testbench/TestBenchTestCaseTest.java
+++ test/src/com/vaadin/testbench/TestBenchTestCaseTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.easymock.EasyMock;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.InvalidSelectorException;
@@ -18,20 +16,7 @@ import com.gargoylesoftware.htmlunit.ElementNotFoundException;
 /**
  * @author Jonatan Kronqvist / Vaadin Ltd
  */
-public class TestBenchTestCaseTest {
-
-    private TestBenchTestCase testBenchTestCase;
-
-    @Before
-    public void setUp() {
-        testBenchTestCase = new TestBenchTestCase() {
-        };
-    }
-
-    @After
-    public void tareDown() {
-
-    }
+public class TestBenchTestCaseTest extends TestBenchTestCase {
 
     private WebDriver mockWebDriverFindElementToReturnOrThrow(
             Object toReturnOrThrow) {
@@ -53,62 +38,49 @@ public class TestBenchTestCaseTest {
 
     @Test
     public void testIsElementPresent_elementFound() {
-        testBenchTestCase
-                .setDriver(mockWebDriverFindElementToReturnOrThrow(EasyMock
-                        .createNiceMock(WebElement.class)));
-        assertTrue(testBenchTestCase.isElementPresent(By.id("thisIDExists")));
+        setDriver(mockWebDriverFindElementToReturnOrThrow(EasyMock
+                .createNiceMock(WebElement.class)));
+        assertTrue(isElementPresent(By.id("thisIDExists")));
     }
 
     @Test
     public void testIsElementPresent_elementNotFoundThrowsNoSuchElementException() {
-        testBenchTestCase
-                .setDriver(mockWebDriverFindElementToReturnOrThrow(new NoSuchElementException(
-                        "")));
-        assertFalse(testBenchTestCase.isElementPresent(By
-                .id("thisIDDoesNotExist")));
+        setDriver(mockWebDriverFindElementToReturnOrThrow(new NoSuchElementException(
+                "")));
+        assertFalse(isElementPresent(By.id("thisIDDoesNotExist")));
     }
 
     @Test
     public void testIsElementPresent_elementNotFoundThrowsInvalidSelectorException() {
-        testBenchTestCase
-                .setDriver(mockWebDriverFindElementToReturnOrThrow(new InvalidSelectorException(
-                        "")));
-        assertFalse(testBenchTestCase.isElementPresent(By
-                .id("thisIDDoesNotExist")));
+        setDriver(mockWebDriverFindElementToReturnOrThrow(new InvalidSelectorException(
+                "")));
+        assertFalse(isElementPresent(By.id("thisIDDoesNotExist")));
     }
 
     @Test
     public void testIsElementPresent_elementNotFoundThrowsElementNotVisibleException() {
-        testBenchTestCase
-                .setDriver(mockWebDriverFindElementToReturnOrThrow(new ElementNotVisibleException(
-                        "")));
-        assertFalse(testBenchTestCase.isElementPresent(By
-                .id("thisElementIsNotVisible")));
+        setDriver(mockWebDriverFindElementToReturnOrThrow(new ElementNotVisibleException(
+                "")));
+        assertFalse(isElementPresent(By.id("thisElementIsNotVisible")));
     }
 
     @Test
     public void testIsElementPresent_elementNotFoundThrowsElementNotFoundException() {
-        testBenchTestCase
-                .setDriver(mockWebDriverFindElementToReturnOrThrow(new ElementNotFoundException(
-                        "", "", "")));
-        assertFalse(testBenchTestCase.isElementPresent(By
-                .id("thisIDDoesNotExist")));
+        setDriver(mockWebDriverFindElementToReturnOrThrow(new ElementNotFoundException(
+                "", "", "")));
+        assertFalse(isElementPresent(By.id("thisIDDoesNotExist")));
     }
 
     @Test
     public void testIsElementPresent_elementNotFoundThrowsNPE() {
-        testBenchTestCase
-                .setDriver(mockWebDriverFindElementToReturnOrThrow(new NullPointerException(
-                        "")));
-        assertFalse(testBenchTestCase.isElementPresent(By
-                .id("thisIDDoesNotExist")));
+        setDriver(mockWebDriverFindElementToReturnOrThrow(new NullPointerException(
+                "")));
+        assertFalse(isElementPresent(By.id("thisIDDoesNotExist")));
     }
 
     @Test
     public void testIsElementPresent_elementNotFoundReturnsNull() {
-        testBenchTestCase
-                .setDriver(mockWebDriverFindElementToReturnOrThrow(null));
-        assertFalse(testBenchTestCase.isElementPresent(By
-                .id("thisIDDoesNotExist")));
+        setDriver(mockWebDriverFindElementToReturnOrThrow(null));
+        assertFalse(isElementPresent(By.id("thisIDDoesNotExist")));
     }
 }
