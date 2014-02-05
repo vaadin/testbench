e76e7c004406f141271f80c9c691b494eaa9a778
Teemu Suo-Anttila <teemusa@vaadin.com>
2014-02-05 17:10:06 +0200

    Implement TabSheet and Accordion Element APIs (#13104)
    Change-Id: I990042bdc084edcdf16ef69b6e1b8243e01233dc

--- vaadin-testbench/src/main/java/com/vaadin/testbench/elements/AccordionElement.java
+++ vaadin-testbench/src/main/java/com/vaadin/testbench/elements/AccordionElement.java
@@ -1,6 +1,14 @@
 package com.vaadin.testbench.elements;
 
+import org.openqa.selenium.By;
+
 @ServerClass("com.vaadin.ui.Accordion")
 public class AccordionElement extends TabSheetElement {
+
+    {
+        // Only difference in using Accordion vs. TabSheet is CSS class name for
+        // items
+        byTabCell = By.className("v-accordion-item");
+    }
 
 }
--- vaadin-testbench/src/main/java/com/vaadin/testbench/elements/TabSheetElement.java
+++ vaadin-testbench/src/main/java/com/vaadin/testbench/elements/TabSheetElement.java
@@ -1,6 +1,83 @@
 package com.vaadin.testbench.elements;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.testbench.By;
+import com.vaadin.testbench.TestBench;
+
 @ServerClass("com.vaadin.ui.TabSheet")
 public class TabSheetElement extends AbstractComponentContainerElement {
 
+    protected org.openqa.selenium.By byTabCell = By
+            .className("v-tabsheet-tabitem");
+    private static org.openqa.selenium.By byCaption = By
+            .className("v-captiontext");
+    private static org.openqa.selenium.By byClosable = By
+            .className("v-tabsheet-caption-close");
+
+    /**
+     * Gets a list of Tabs inside the Tab container.
+     * 
+     * @return List of tabs
+     */
+    public List<String> getTabCaptions() {
+        List<String> tabCaptions = new ArrayList<String>();
+        for (WebElement tab : findElements(byTabCell)) {
+            tabCaptions.add(tab.findElement(byCaption).getText());
+        }
+        return tabCaptions;
+    }
+
+    /**
+     * Opens a Tab that has caption equal to given tabCaption.
+     * 
+     * @param tabCaption
+     *            Caption of the tab to be opened
+     */
+    public void openTab(String tabCaption) {
+        for (WebElement tabCell : findElements(byTabCell)) {
+            WebElement tab = tabCell.findElement(byCaption);
+            if (tabCaption.equals(tab.getText())) {
+                tab.click();
+            }
+        }
+    }
+
+    /**
+     * If tab with given caption is closable, closes it.
+     * 
+     * @param tabCaption
+     *            Caption of the tab to be opened
+     */
+    public void closeTab(String tabCaption) {
+        for (WebElement tabCell : findElements(byTabCell)) {
+            WebElement tab = tabCell.findElement(byCaption);
+            if (tabCaption.equals(tab.getText())) {
+                try {
+                    tabCell.findElement(byClosable).click();
+                    // Going further causes a StaleElementReferenceException.
+                    return;
+                } catch (NoSuchElementException e) {
+                    // Do nothing.
+                }
+            }
+        }
+    }
+
+    /**
+     * Gets TabSheet content and wraps it in given class.
+     * 
+     * @param clazz
+     *            Components element class
+     * @return TabSheet content wrapped in given class
+     */
+    public <T extends AbstractElement> T getContent(Class<T> clazz) {
+        return TestBench.createElement(clazz,
+                $$(AbstractComponentElement.class).first().getWrappedElement(),
+                getCommandExecutor());
+    }
 }
