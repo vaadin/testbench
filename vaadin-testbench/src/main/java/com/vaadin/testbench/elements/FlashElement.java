25f4713e2dcbcfcd1913a035beb20cb0770423de
Jonatan Kronqvist <jonatan@vaadin.com>
2014-02-13 14:47:17 +0200

    Update license information to CVALv3 (#12880)
    - New license files
    - Updated the license plugin to the latest version
    - Updated the headers of source files to CVALv3 using the license plugin
    
    Change-Id: Iaa7194a40832564c589deed89de252efee417295

--- vaadin-testbench/src/main/java/com/vaadin/testbench/elements/FlashElement.java
+++ vaadin-testbench/src/main/java/com/vaadin/testbench/elements/FlashElement.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (C) 2012 Vaadin Ltd
+ *
+ * This program is available under Commercial Vaadin Add-On License 3.0
+ * (CVALv3).
+ *
+ * See the file licensing.txt distributed with this software for more
+ * information about licensing.
+ *
+ * You should have received a copy of the license along with this program.
+ * If not, see <http://vaadin.com/license/cval-3.0>.
+ */
 package com.vaadin.testbench.elements;
 
 @ServerClass("com.vaadin.ui.Flash")
--- vaadin-testbench/src/test/java/com/vaadin/testbench/screenshot/ScreenshotMaskingTest.java
+++ vaadin-testbench/src/test/java/com/vaadin/testbench/screenshot/ScreenshotMaskingTest.java
@@ -1,16 +1,14 @@
 /**
  * Copyright (C) 2012 Vaadin Ltd
  *
- * This program is available under Commercial Vaadin Add-On License 2.0
- * (CVALv2) or GNU Affero General Public License (version 3 or later at
- * your option).
+ * This program is available under Commercial Vaadin Add-On License 3.0
+ * (CVALv3).
  *
  * See the file licensing.txt distributed with this software for more
  * information about licensing.
  *
  * You should have received a copy of the license along with this program.
- * If not, see <http://vaadin.com/license/cval-2.0> or
- * <http://www.gnu.org/licenses> respectively.
+ * If not, see <http://vaadin.com/license/cval-3.0>.
  */
 package com.vaadin.testbench.screenshot;
 
