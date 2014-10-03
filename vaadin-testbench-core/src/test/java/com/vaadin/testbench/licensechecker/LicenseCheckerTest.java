bbc5fc40cdd476706338159a5e0d54a7eed25adf
Mika Murtojarvi <mika@vaadin.com>
2014-10-03 12:57:25 +0300

    Use the same line change characters in LicenseCheckerTest and LicenseChecker.
    This resolves a test failure on Windows machines.
    
    Change-Id: I7e332bea302f9cb1311b94f470775d7c212dfc9d

--- vaadin-testbench-core/src/test/java/com/vaadin/testbench/licensechecker/LicenseCheckerTest.java
+++ vaadin-testbench-core/src/test/java/com/vaadin/testbench/licensechecker/LicenseCheckerTest.java
@@ -20,6 +20,7 @@ public class LicenseCheckerTest {
     private final static String DEVELOPER_KEY = "0282c076-d6e1-4a11-9092-d36285b5a6f9";
     private final static String SUBSCRIPTION_KEY_EXPIRED = "9fa746f1-028e-4f02-823a-d022c29119e2";
     private final static String INVALID_KEY = "1111111111111111111111111111";
+    private static String NEW_LINE = System.getProperty("line.separator");
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
@@ -38,7 +39,9 @@ public class LicenseCheckerTest {
         String output = outContent.toString();
         // output =
         // "-------------Vaadin TestBench 4 registered to dmitrii@vaadin.com (Pro Tools subscription).------------------";
-        String pattern = "-*\nVaadin TestBench 4 registered to .*\n-*(\n)*";
+        String pattern = "-*" + NEW_LINE
+                + "Vaadin TestBench 4 registered to .*" + NEW_LINE + "-*("
+                + NEW_LINE + ")*";
         Assert.assertTrue(output.matches(pattern));
     }
 
@@ -47,9 +50,12 @@ public class LicenseCheckerTest {
         Properties props = System.getProperties();
         props.setProperty("vaadin.testbench.developer.license",
                 SUBSCRIPTION_KEY_EXPIRED);
-        String expectingMessage = "----------------------------------------------------------------------------------\n"
-                + "License for Vaadin TestBench 4 has expired. Get a valid license at vaadin.com/pro\n"
-                + "----------------------------------------------------------------------------------\n";
+        String expectingMessage = "----------------------------------------------------------------------------------"
+                + NEW_LINE
+                + "License for Vaadin TestBench 4 has expired. Get a valid license at vaadin.com/pro"
+                + NEW_LINE
+                + "----------------------------------------------------------------------------------"
+                + NEW_LINE;
 
         exception.expect(Error.class);
         exception.expectMessage(expectingMessage);
@@ -61,9 +67,12 @@ public class LicenseCheckerTest {
     public void testNoLicense() {
         Properties props = System.getProperties();
         props.setProperty("vaadin.testbench.developer.license", "");
-        String expectingMessage = "---------------------------------------------------------------------------------\n"
-                + "License for Vaadin TestBench 4 not found. Go to vaadin.com/pro for more details.\n"
-                + "---------------------------------------------------------------------------------\n";
+        String expectingMessage = "---------------------------------------------------------------------------------"
+                + NEW_LINE
+                + "License for Vaadin TestBench 4 not found. Go to vaadin.com/pro for more details."
+                + NEW_LINE
+                + "---------------------------------------------------------------------------------"
+                + NEW_LINE;
         exception.expect(Error.class);
         exception.expectMessage(expectingMessage);
         LicenseChecker.nag();
@@ -73,9 +82,12 @@ public class LicenseCheckerTest {
     public void testInvalidLicense() {
         Properties props = System.getProperties();
         props.setProperty("vaadin.testbench.developer.license", INVALID_KEY);
-        String expectingMessage = "-------------------------------------------------------------------------------------\n"
-                + "License for Vaadin TestBench 4 is not valid. Get a valid license from vaadin.com/pro\n"
-                + "-------------------------------------------------------------------------------------\n";
+        String expectingMessage = "-------------------------------------------------------------------------------------"
+                + NEW_LINE
+                + "License for Vaadin TestBench 4 is not valid. Get a valid license from vaadin.com/pro"
+                + NEW_LINE
+                + "-------------------------------------------------------------------------------------"
+                + NEW_LINE;
         exception.expect(Error.class);
         exception.expectMessage(expectingMessage);
         LicenseChecker.nag();
