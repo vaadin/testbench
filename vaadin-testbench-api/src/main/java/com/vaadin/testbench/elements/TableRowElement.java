ff4f3ae54a1cc82d9b8a7890ee487e7298ce70a6
Dmitrii Rogozin <dmitrii@vaadin.com>
2014-10-22 16:52:30 +0300

    Remove getElementInCell from TableRowElement class (#15085)
    Change-Id: I80c19df20b53d3a73757a15b964721eb5c2e28cd

--- vaadin-testbench-api/src/main/java/com/vaadin/testbench/elements/TableRowElement.java
+++ vaadin-testbench-api/src/main/java/com/vaadin/testbench/elements/TableRowElement.java
@@ -30,32 +30,4 @@ public class TableRowElement extends AbstractComponentElement {
         return wrapElement(cellContent.findElement(By.xpath("./*")),
                 getCommandExecutor());
     }
-
-    /**
-     * Returns the element in the cell, wrapped to the provided Element type.
-     *
-     * <p>
-     * For example getElementInCell(1,ButtonElement.class) will return a
-     * ButtonElement, getElementInCell(1,ComboBoxElement.class) ComboBoxElement
-     * and so on.
-     *
-     * @param col
-     *            column index
-     * @param elementClass
-     *            type to which result will be wrapped
-     * @return the element in the cell, wrapped to the provided Element type.
-     */
-    public <T extends AbstractComponentElement> T getElementInCell(int col,
-            Class<T> elementClass) {
-        WebElement cell = getCell(col);
-        List<WebElement> elements = cell.findElements(By.xpath("./*"));
-        if (elements.size() == 0) {
-            return null;
-        }
-        WebElement elem = elements.get(0);
-        TestBenchElement tbElem = TestBenchElement.wrapElement(elem,
-                getCommandExecutor());
-
-        return tbElem.wrap(elementClass);
-    }
 }
