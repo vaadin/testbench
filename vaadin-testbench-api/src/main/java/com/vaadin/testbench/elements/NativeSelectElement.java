3f9d8a75fa4013a8eaa9ad52c514ebcc3d5fa387
Dmitrii Rogozin <dmitrii@vaadin.com>
2014-10-22 17:19:51 +0300

    Modify API for NativeSelectElement (#15093)
    Remove deselectByText
    Remove getAllSelectedOptions
    Remove getFirstSelectedOption
    Remove isMultiple
    
    Change-Id: Ic0f79e2e3f40740eac4389a9c53ff3a601853a2d

--- vaadin-testbench-api/src/main/java/com/vaadin/testbench/elements/NativeSelectElement.java
+++ vaadin-testbench-api/src/main/java/com/vaadin/testbench/elements/NativeSelectElement.java
@@ -30,26 +30,8 @@ public class NativeSelectElement extends AbstractSelectElement {
         selectElement = new Select(findElement(By.tagName("select")));
     }
 
-    public void deselectByText(String text) {
-        selectElement.deselectByVisibleText(text);
-    }
-
-    public List<TestBenchElement> getAllSelectedOptions() {
-        return wrapElements(selectElement.getAllSelectedOptions(),
-                getCommandExecutor());
-    }
-
-    public TestBenchElement getFirstSelectedOption() {
-        return wrapElement(selectElement.getFirstSelectedOption(),
-                getCommandExecutor());
-    }
-
     public List<TestBenchElement> getOptions() {
         return wrapElements(selectElement.getOptions(), getCommandExecutor());
-    }
-
-    public boolean isMultiple() {
-        return selectElement.isMultiple();
     }
 
     public void selectByText(String text) throws ReadOnlyException {
