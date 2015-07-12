5375e6537df4a3b38dba0a70897b56596068212c
Artur Signell <artur@vaadin.com>
2015-07-12 20:36:56 +0300

    Make ServerClass @inherited to make sub classing customizations easier (#15378)
    Change-Id: I770d3af4a087258ed55ad420b20c7d621c152435

--- vaadin-testbench-core/src/main/java/com/vaadin/testbench/elementsbase/ServerClass.java
+++ vaadin-testbench-core/src/main/java/com/vaadin/testbench/elementsbase/ServerClass.java
@@ -13,6 +13,7 @@
 package com.vaadin.testbench.elementsbase;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -26,6 +27,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Inherited
 public @interface ServerClass {
 
     String value();
