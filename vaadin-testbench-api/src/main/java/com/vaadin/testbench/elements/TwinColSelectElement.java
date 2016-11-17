806452a874c6bc2262b7e8dfb64fe09f857c6e34
Guillermo Alvarez <guillermo@vaadin.com>
2016-11-17 18:11:52 +0200

    Add API to getAvailableOptions in TwinColSelectElement

--- vaadin-testbench-api/src/main/java/com/vaadin/testbench/elements/TwinColSelectElement.java
+++ vaadin-testbench-api/src/main/java/com/vaadin/testbench/elements/TwinColSelectElement.java
@@ -86,6 +86,15 @@ public class TwinColSelectElement extends AbstractSelectElement {
         return optionTexts;
     }
 
+    /**
+     * Functionality to find available option texts.
+     *
+     * @return List of visible text for available options
+     */
+    public List<String> getAvailableOptions() {
+        return getOptionsFromSelect(options);
+    }
+
     public void selectByText(String text) {
         options.selectByVisibleText(text);
         selButton.click();
