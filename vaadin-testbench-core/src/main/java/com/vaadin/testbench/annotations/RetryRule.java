fd0faaefaa20a523ea205fe462d4e5ee1fefe4cf
rogozinds <rogozinds@gmail.com>
2016-12-08 17:25:53 +0200

    Add RetryRule
    RetryTestRunner allows to run tests several times.
    If tests passes at least once it is mared as passed.
    That can be useful to prevent random failures.

---
+++ vaadin-testbench-core/src/main/java/com/vaadin/testbench/annotations/RetryRule.java
@@ -0,0 +1,39 @@
+package com.vaadin.testbench.annotations;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+public class RetryRule implements TestRule {
+    private int retryCount;
+
+    public RetryRule(int retryCount) {
+        this.retryCount = retryCount;
+    }
+
+    public Statement apply(Statement base, Description description) {
+        return statement(base, description);
+    }
+
+    private Statement statement(final Statement base, final Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                Throwable caughtThrowable = null;
+                // implement retry logic here
+                for (int i = 0; i < retryCount; i++) {
+                    try {
+                        base.evaluate();
+                        return;
+                    } catch (Throwable t) {
+                        caughtThrowable = t;
+                    }
+                }
+                System.err.println(String.format("%s: run failed %s times",
+                        description.getDisplayName(),
+                        retryCount));
+                throw caughtThrowable;
+            }
+        };
+    }
+}
