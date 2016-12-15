8a0da2767b666e3ed077cc302c25c2ed61ae3f24
rogozinds <rogozinds@gmail.com>
2016-12-15 05:27:49 -0800

    Add RetryRule TB-81. (#852)
    If tests passes at least once it is marked as passed.
    That can be useful to prevent random failures.

---
+++ vaadin-testbench-core/src/test/java/com/vaadin/testbench/RetryRuleAfterTest.java
@@ -0,0 +1,25 @@
+package com.vaadin.testbench;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+
+public class RetryRuleAfterTest {
+    private int afterExecutedCount = 0;
+
+    @Rule
+    public RetryRule retry = new RetryRule(3);
+
+    @After
+    public void cleanUp() {
+        afterExecutedCount++;
+    }
+
+    @Test
+    public void afterAnnotation_executedSeveralTimes_whenUsingRetryRule() {
+        Assert.assertEquals(2, afterExecutedCount);
+    }
+
+}
