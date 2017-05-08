902cc4d8a644abbf3eb90a4660413f47a8c827c8
Sven Ruppert <sven.ruppert@gmail.com>
2017-05-08 13:32:01 +0200

    [TB-96] - Testbench AddOn to switch SeleniumHub - fix broken Build on TC

--- vaadin-testbench-integration-tests/src/test/java/com/vaadin/tests/elements/ng/BaseParallelTest.java
+++ vaadin-testbench-integration-tests/src/test/java/com/vaadin/tests/elements/ng/BaseParallelTest.java
@@ -4,29 +4,25 @@ import static com.vaadin.tests.elements.ng.tooling.BrowserDriverFunctions.readBr
 import java.util.Collections;
 import java.util.List;
 
-import org.junit.runner.RunWith;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.vaadin.testbench.TestBenchTestCase;
-import com.vaadin.testbench.annotations.BrowserConfiguration;
-import com.vaadin.testbench.parallel.ParallelRunner;
 
 /**
  *
  */
-@RunWith(ParallelRunner.class)
+// prepare @RunWith(ParallelRunner.class)
 public class BaseParallelTest extends TestBenchTestCase {
 
     public BaseParallelTest() {
         throw new RuntimeException("not unsable right now");
     }
 
-    @BrowserConfiguration
+    // prepare @BrowserConfiguration
     public List<DesiredCapabilities> getBrowserConfiguration() {
         return readBrowserCombinations
             .get()
             .getOrElse(Collections::emptyList);
     }
-
 
 }
