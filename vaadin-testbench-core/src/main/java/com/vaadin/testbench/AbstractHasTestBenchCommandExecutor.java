2a9658c4ab229647afdf8af2bc4c33195c65a8a5
Artur Signell <artur@vaadin.com>
2017-10-09 13:34:53 +0300

    Rewrite ElementQuery to use querySelectorAll and look inside the shadow root of elements
    Adds waitForFirst() to make it easier to handle page or delayed navigation where waitForVaadin will not work
    Adds onPage() to search starting from the root of the page instead of inside the given element
    Adds attribute search to be able to find e.g. a paper-tab based on page-id

--- vaadin-testbench-core/src/main/java/com/vaadin/testbench/AbstractHasTestBenchCommandExecutor.java
+++ vaadin-testbench-core/src/main/java/com/vaadin/testbench/AbstractHasTestBenchCommandExecutor.java
@@ -12,15 +12,13 @@
  */
 package com.vaadin.testbench;
 
-import com.vaadin.testbench.elementsbase.AbstractElement;
-
 /**
  * Abstract class that provides useful Element finding functions. Extended in
  * {@link TestBenchElement} and {@link TestBenchTestCase}. These classes have
  * their own search contexts to manage scoping element searches where needed.
  */
-public abstract class AbstractHasTestBenchCommandExecutor implements
-        HasTestBenchCommandExecutor {
+public abstract class AbstractHasTestBenchCommandExecutor
+        implements HasTestBenchCommandExecutor {
 
     /**
      * Prepare a {@link ElementQuery} instance to use for locating components on
@@ -28,45 +26,27 @@ public abstract class AbstractHasTestBenchCommandExecutor implements
      * the sought-after object. If this function gets called through an element,
      * it uses the element as its search context. Otherwise the search context
      * is the driver.
-     * 
+     *
      * @return an appropriate {@link ElementQuery} instance
      */
-    public <T extends AbstractElement> ElementQuery<T> $(Class<T> clazz) {
+    public <T extends TestBenchElement> ElementQuery<T> $(Class<T> clazz) {
         return new ElementQuery<T>(clazz).context(getContext());
     }
 
     /**
-     * Prepare a {@link ElementQuery} instance to use for locating components on
-     * the client. The returned object can be manipulated to uniquely identify
-     * the sought-after object. If this function gets called through an element,
-     * it uses the element as its search context. Otherwise the search context
-     * is the driver.
-     * 
-     * This search is not recursive and can find the given hierarchy only if it
-     * can be found as direct children of given context. The same can be done
-     * with {@code $(Foo.class).recursive(false) }
-     * 
-     * @return an appropriate {@link ElementQuery} instance
-     */
-    public <T extends AbstractElement> ElementQuery<T> $$(Class<T> clazz) {
-        return new ElementQuery<T>(clazz).context(getContext())
-                .recursive(false);
-    }
-
-    /**
      * Returns true if a component can be found.
-     * 
+     *
      * @param clazz
-     *            AbstractElement subclass representing a Vaadin component
+     *            TestBenchElement subclass representing a Vaadin component
      * @return true if component of given class can be found
      */
-    public boolean isElementPresent(Class<? extends AbstractElement> clazz) {
+    public boolean isElementPresent(Class<? extends TestBenchElement> clazz) {
         return !$(clazz).all().isEmpty();
     }
 
     /**
      * Returns true if a component can be found with given By selector.
-     * 
+     *
      * @param by
      *            the selector used to find element
      * @return true if the component can be found
