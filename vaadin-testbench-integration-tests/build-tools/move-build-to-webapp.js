dc9b5b670852e3a6f9be9d8304e9d4c0980b1007
Guille <alvarezguille@users.noreply.github.com>
2017-11-22 18:37:30 +0200

    Remove unneeded dependency

--- vaadin-testbench-integration-tests/build-tools/move-build-to-webapp.js
+++ vaadin-testbench-integration-tests/build-tools/move-build-to-webapp.js
@@ -3,14 +3,7 @@
 // Move files from the "build" directory to the target dir
 
 const rimraf = require('rimraf');
-const mkdirp = require('mkdirp');
 const fs = require('fs');
-
-const frontendVaadin = "../../../#frontend-target-folder#";
-
-if (!fs.existsSync(frontendVaadin)) {
-	mkdirp.sync(frontendVaadin);
-}
 
 for (dir of ["frontend-es5","frontend-es6"]) {
     const sourceDir = "build/" + dir;
