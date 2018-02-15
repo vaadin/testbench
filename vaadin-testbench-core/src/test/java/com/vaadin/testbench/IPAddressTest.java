d81085984205daba6a3dba4c1418d6586977f524
Artur <artur@vaadin.com>
2018-02-15 10:12:41 +0200

    Add helper for finding local IP address (#994)

---
+++ vaadin-testbench-core/src/test/java/com/vaadin/testbench/IPAddressTest.java
@@ -0,0 +1,24 @@
+package com.vaadin.testbench;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IPAddressTest {
+
+    @Test
+    public void siteLocalAddress() {
+        // Can't know if the machine running the test actually has a site local
+        // address...
+        try {
+            String address = IPAddress.findSiteLocalAddress();
+            Assert.assertTrue(
+                    address.startsWith("10.") || address.startsWith("172.16.")
+                            || address.startsWith("192.168."));
+        } catch (RuntimeException e) {
+            Assert.assertEquals(
+                    "No compatible (10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16) IP address found.",
+                    e.getMessage());
+        }
+    }
+
+}
