218fe18e3923879af30e24a22342af5f0978339b
Guille <alvarezguille@users.noreply.github.com>
2018-04-16 09:46:34 +0300

    Make CyclicObjectWorkaround specific for firefox (#1050)
    Also consider protected properties

--- vaadin-testbench-core/src/main/resources/com/vaadin/testbench/cyclic-object-workaround.js
+++ vaadin-testbench-core/src/main/resources/com/vaadin/testbench/cyclic-object-workaround.js
@@ -1,13 +1,16 @@
-if (jsObject && jsObject.forEach) {
-	jsObject.forEach(function(e) {
-		Object.keys(e).filter(
-			function(k) {
-				return e.hasOwnProperty(k)
-						&& (k.indexOf('__') == 0 || k == '$' || k == 'X') && e[k];
-		}).forEach(function(key) {
-				e[key].toJSON = function() {
-					return;
-				}
+if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1){
+	if (jsObject && jsObject.forEach) {
+		jsObject.forEach(function(e) {
+			Object.keys(e).filter(
+				function(k) {
+					return e.hasOwnProperty(k)
+							&& (k.indexOf('_') == 0 || k == '$' || k == 'X') && e[k];
+			}).forEach(function(key) {
+					e[key].toJSON = function() {
+						return;
+					}
+			});
 		});
-	});
-};
+	};
+}
+
