6b218c46b2adca3348962635c253a970f35158cc
Guille <alvarezguille@users.noreply.github.com>
2018-08-29 16:07:05 +0300

    Backport support for RetryRule to TB 4.3 (#1114)
    * Backport support for RetryRule to TB 4.3
    
    Picked from #852 and #874
    Fixes #1113

---
+++ vaadin-testbench-core/src/test/java/com/vaadin/testbench/RetryRuleTestbenchTest.java
@@ -0,0 +1,16 @@
+package com.vaadin.testbench;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RetryRuleTestbenchTest extends TestBenchTestCase {
+
+    private int count = 0;
+
+    @Test(expected = AssertionError.class)
+    public void defaultExecution_noRetryRule_testcaseRunOnce() {
+        count++;
+        Assert.assertEquals(2, count);
+    }
+
+}
