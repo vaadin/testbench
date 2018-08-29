ddec44cdc407a7b9f0d7351eb781e8a86684c83e
alexberazouski <alex@vaadin.com>
2018-08-29 17:22:24 +0300

    Fix indentation (#1116)

--- vaadin-testbench-core/src/test/java/com/vaadin/testbench/ParentRetryRuleTest.java
+++ vaadin-testbench-core/src/test/java/com/vaadin/testbench/ParentRetryRuleTest.java
@@ -7,14 +7,14 @@ import org.junit.Test;
 import com.vaadin.testbench.testutils.ParentRetryRule;
 
 public class ParentRetryRuleTest extends ParentRetryRule {
-	private int count = 0;
-	//Override parent RetryRule that uses RetryRule(1)
-	@Rule
-	public RetryRule retry = new RetryRule(2);
+    private int count = 0;
+    //Override parent RetryRule that uses RetryRule(1)
+    @Rule
+    public RetryRule retry = new RetryRule(2);
 
-	@Test
-	public void parentRule_settingRule_overridesParentRule() {
-		count++;
-		Assert.assertEquals(2, count);
-	}
+    @Test
+    public void parentRule_settingRule_overridesParentRule() {
+        count++;
+        Assert.assertEquals(2, count);
+    }
 }
