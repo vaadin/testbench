c210ca81ed7e9ae8bce08473a620184d7e810d24
Sven Ruppert <sven.ruppert@gmail.com>
2019-02-19 10:14:25 +0100

    start cleaning up the repo for restart

--- vaadin-testbench-integration-tests/src/test/java/com/vaadin/tests/MyComponentWithIdElement.java
+++
@@ -1,9 +0,0 @@
-package com.vaadin.tests;
-
-import com.vaadin.testbench.TestBenchElement;
-import com.vaadin.testbench.annotations.Attribute;
-
-@Attribute(name = "id", value = Attribute.SIMPLE_CLASS_NAME)
-public class MyComponentWithIdElement extends TestBenchElement {
-
-}
