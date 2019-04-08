b53be5a2f1c69ce8f436ed4e58e25713355a0cc8
Sayo Oladeji <sayo@vaadin.com>
2019-04-08 11:57:33 +0300

    Add license header to sources

--- vaadin-testbench-core/src/main/java/com/vaadin/testbench/ElementQuery.java
+++ vaadin-testbench-core/src/main/java/com/vaadin/testbench/ElementQuery.java
@@ -1,5 +1,24 @@
 package com.vaadin.testbench;
 
+/*-
+ * #%L
+ * vaadin-testbench-core
+ * %%
+ * Copyright (C) 2019 Vaadin Ltd
+ * %%
+ * Copyright (C) ${year} Vaadin Ltd
+ * 
+ * This program is available under Commercial Vaadin Add-On License 3.0
+ * (CVALv3).
+ * 
+ * See the file licensing.txt distributed with this software for more
+ * information about licensing.
+ * 
+ * You should have received a copy of the license along with this program.
+ * If not, see <http://vaadin.com/license/cval-3>.
+ * #L%
+ */
+
 import com.vaadin.testbench.annotations.Attribute;
 import com.vaadin.testbench.elementsbase.Element;
 import com.vaadin.testbench.internal.SharedUtil;
--- vaadin-testbench-core/src/main/java/com/vaadin/testbench/addons/framework/Registration.java
+++ vaadin-testbench-core/src/main/java/com/vaadin/testbench/addons/framework/Registration.java
@@ -1,5 +1,24 @@
 package com.vaadin.testbench.addons.framework;
 
+/*-
+ * #%L
+ * vaadin-testbench-core
+ * %%
+ * Copyright (C) 2019 Vaadin Ltd
+ * %%
+ * Copyright (C) ${year} Vaadin Ltd
+ * 
+ * This program is available under Commercial Vaadin Add-On License 3.0
+ * (CVALv3).
+ * 
+ * See the file licensing.txt distributed with this software for more
+ * information about licensing.
+ * 
+ * You should have received a copy of the license along with this program.
+ * If not, see <http://vaadin.com/license/cval-3>.
+ * #L%
+ */
+
 import java.io.Serializable;
 
 @FunctionalInterface
