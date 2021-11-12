vaadin/testbench

commit 37c9c5d255184e30297bd6342fd0efb49ee1d5b2

fix: Do not wait if no Flow connectors found (#1296)
Fixes https://github.com/vaadin/testbench/issues/1294

vaadin-testbench-core/src/main/java/com/vaadin/testbench/commands/TestBenchCommandExecutor.java
@@ -50,21 +50,16 @@ public class TestBenchCommandExecutor implements TestBenchCommands, HasDriver {
     private boolean autoScrollIntoView = true;
     // @formatter:off
     String WAIT_FOR_VAADIN_SCRIPT =
-            "if (!window.Vaadin || !window.Vaadin.Flow) {"
-            + "  return true;"
-            + "}"
-            + "var clients = window.Vaadin.Flow.clients;"
-            + "if (clients) {"
+            "if (window.Vaadin && window.Vaadin.Flow && window.Vaadin.Flow.clients) {"
+            + "  var clients = window.Vaadin.Flow.clients;"
             + "  for (var client in clients) {"
             + "    if (clients[client].isActive()) {"
             + "      return false;"
             + "    }"
             + "  }"
             + "  return true;"
-            + "} else {" +
-            // A Vaadin connector was found so this is most likely a Vaadin
-            // application. Keep waiting.
-            "  return false;"
+            + "} else {"
+            + "  return true;"
             + "}";
     // @formatter:on
 

vaadin-testbench-integration-tests/src/test/java/com/vaadin/tests/WaitForVaadinIT.java
@@ -1,0 +1,78 @@
+/**
+ * Copyright (C) 2021 Vaadin Ltd
+ *
+ * This program is available under Commercial Vaadin Developer License
+ * 4.0 (CVDLv4).
+ *
+ *
+ * For the full License, see <https://vaadin.com/license/cvdl-4.0>.
+ */
+package com.vaadin.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.testUI.PageObjectView;
+
+public class WaitForVaadinIT extends AbstractTB6Test {
+
+    private static final long NON_BLOCKING_EXECUTION_TIMEOUT = 5000;
+    private static final long BLOCKING_EXECUTION_TIMEOUT = 20000;
+
+    @Override
+    protected Class<? extends Component> getTestView() {
+        return PageObjectView.class;
+    }
+
+    @Test
+    public void waitForVaadin_connectorsInitialised_returnsImmediately() {
+        openTestURL();
+        assertExecutionNoLonger(() -> getCommandExecutor().waitForVaadin());
+    }
+
+    @Test
+    public void waitForVaadin_activeConnector_waits() {
+        openTestURL();
+        getCommandExecutor().executeScript(
+                "window.Vaadin.Flow.clients[\"blocker\"] = {isActive: () => true};");
+        assertExecutionBlocked(() -> getCommandExecutor().waitForVaadin());
+    }
+
+    @Test
+    public void waitForVaadin_noConnectors_returnsImmediately() {
+        openTestURL();
+        getCommandExecutor().executeScript(
+                "window.Vaadin.Flow.clients = undefined;");
+        assertExecutionNoLonger(() -> getCommandExecutor().waitForVaadin());
+    }
+
+    @Test
+    public void waitForVaadin_noFlow_returnsImmediately() {
+        openTestURL();
+
+        getCommandExecutor().executeScript(
+                "window.Vaadin.Flow = undefined;");
+        assertExecutionNoLonger(() -> getCommandExecutor().waitForVaadin());
+    }
+
+    private void assertExecutionNoLonger(Runnable command) {
+        long before = System.currentTimeMillis();
+        command.run();
+        long after = System.currentTimeMillis();
+        long timeout = after - before;
+        Assert.assertTrue(
+                "Unexpected execution time, waiting time = " +
+                timeout, timeout < NON_BLOCKING_EXECUTION_TIMEOUT);
+    }
+
+    private void assertExecutionBlocked(Runnable command) {
+        long before = System.currentTimeMillis();
+        command.run();
+        long after = System.currentTimeMillis();
+        long timeout = after - before;
+        Assert.assertTrue(
+                "Unexpected blocked execution time, waiting time = " +
+                timeout, timeout >= BLOCKING_EXECUTION_TIMEOUT);
+    }
+}

commit b90d75c7a93d6bba85bd56367b53248b50624ad2

fix: Wait for dev mode server to finish (#1304)
Fixes a regression introduced in #1296

vaadin-testbench-core/src/main/java/com/vaadin/testbench/commands/TestBenchCommandExecutor.java
@@ -58,6 +58,8 @@ public class TestBenchCommandExecutor implements TestBenchCommands, HasDriver {
             + "    }"
             + "  }"
             + "  return true;"
+            + "} else if (window.Vaadin && window.Vaadin.Flow && window.Vaadin.Flow.devServerIsNotLoaded) {"
+            + "  return false;"
             + "} else {"
             + "  return true;"
             + "}";

vaadin-testbench-integration-tests/src/test/java/com/vaadin/tests/WaitForVaadinIT.java
@@ -56,6 +56,15 @@ public class WaitForVaadinIT extends AbstractTB6Test {
         assertExecutionNoLonger(() -> getCommandExecutor().waitForVaadin());
     }
 
+    @Test
+    public void waitForVaadin_devModeNotReady_waits() {
+        openTestURL();
+
+        getCommandExecutor().executeScript(
+                "window.Vaadin = {Flow: {devServerIsNotLoaded: true}};");
+        assertExecutionBlocked(() -> getCommandExecutor().waitForVaadin());
+    }
+
     private void assertExecutionNoLonger(Runnable command) {
         long before = System.currentTimeMillis();
         command.run();