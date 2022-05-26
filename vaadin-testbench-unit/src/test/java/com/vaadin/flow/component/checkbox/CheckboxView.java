c86d17fc4c3bc314a02486f6c8d83d0d58eb9c57
Marco Collovati <marco@vaadin.com>
2022-05-26 17:08:33 +0200

    feat: Added CheckboxWrap
    Part of #1371

---
+++ vaadin-testbench-unit/src/test/java/com/vaadin/flow/component/checkbox/CheckboxView.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2022 Vaadin Ltd
+ *
+ * This program is available under Commercial Vaadin Developer License
+ * 4.0 (CVDLv4).
+ *
+ *
+ * For the full License, see <https://vaadin.com/license/cvdl-4.0>.
+ */
+
+package com.vaadin.flow.component.checkbox;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasComponents;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.router.Route;
+
+@Tag("div")
+@Route(value = "checkbox", registerAtStartup = false)
+public class CheckboxView extends Component implements HasComponents {
+
+    Checkbox checkbox = new Checkbox();
+
+    public CheckboxView() {
+        add(checkbox);
+    }
+}
---
+++ vaadin-testbench-unit/src/test/java/com/vaadin/flow/component/checkbox/CheckboxWrapTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2022 Vaadin Ltd
+ *
+ * This program is available under Commercial Vaadin Developer License
+ * 4.0 (CVDLv4).
+ *
+ *
+ * For the full License, see <https://vaadin.com/license/cvdl-4.0>.
+ */
+
+package com.vaadin.flow.component.checkbox;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.vaadin.flow.router.RouteConfiguration;
+import com.vaadin.testbench.unit.UIUnitTest;
+
+class CheckboxWrapTest extends UIUnitTest {
+
+    CheckboxView view;
+    CheckboxWrap<Checkbox> checkbox_;
+
+    @Override
+    protected String scanPackage() {
+        return getClass().getPackageName();
+    }
+
+    @BeforeEach
+    public void registerView() {
+        RouteConfiguration.forApplicationScope()
+                .setAnnotatedRoute(CheckboxView.class);
+        view = navigate(CheckboxView.class);
+        checkbox_ = wrap(view.checkbox);
+    }
+
+    @Test
+    void readOnlyCheckbox_isNotUsable() {
+        view.checkbox.setReadOnly(true);
+        Assertions.assertFalse(checkbox_.isUsable(),
+                "Readonly checkbox should not be usable");
+    }
+
+    @Test
+    void click_usable_valueChanges() {
+        Assertions.assertFalse(view.checkbox.getValue(),
+                "Expecting checkbox initial state not to be checked");
+
+        checkbox_.click();
+        Assertions.assertTrue(view.checkbox.getValue(),
+                "Expecting checkbox to be checked, but was not");
+
+        checkbox_.click();
+        Assertions.assertFalse(view.checkbox.getValue(),
+                "Expecting checkbox not to be checked, but was");
+
+    }
+
+    @Test
+    void click_usable_checkedChangeFired() {
+        AtomicBoolean checkedChange = new AtomicBoolean();
+        view.checkbox.addCheckedChangeListener(ev -> checkedChange.set(true));
+        Assertions.assertFalse(view.checkbox.getValue(),
+                "Expecting checkbox not to be checked, but was");
+
+        checkbox_.click();
+        Assertions.assertTrue(checkedChange.get(),
+                "Expected checked change event to be fired, but was not");
+        Assertions.assertTrue(view.checkbox.getValue(),
+                "Expecting checkbox not to be checked, but was");
+    }
+
+    @Test
+    void click_disabled_throws() {
+        view.checkbox.setEnabled(false);
+        Assertions.assertThrows(IllegalStateException.class, checkbox_::click);
+    }
+
+    @Test
+    void click_invisible_throws() {
+        view.checkbox.setVisible(false);
+        Assertions.assertThrows(IllegalStateException.class, checkbox_::click);
+    }
+
+    @Test
+    void click_readOnly_throws() {
+        view.checkbox.setReadOnly(true);
+        Assertions.assertThrows(IllegalStateException.class, checkbox_::click);
+    }
+
+}
