e7131d9c9a6d5a83060713ea1efee31b01e27cc1
Mikael Grankvist <mikael.grankvist@vaadin.com>
2022-06-02 15:04:54 +0300

    feat: test wrap for datepicker and datetimepicker
    part of #1371

---
+++ vaadin-testbench-unit/src/test/java/com/vaadin/flow/component/datetimepicker/DateTimePickerView.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2022 Vaadin Ltd
+ *
+ * This program is available under Commercial Vaadin Developer License
+ * 4.0 (CVDLv4).
+ *
+ *
+ * For the full License, see <https://vaadin.com/license/cvdl-4.0>.
+ */
+package com.vaadin.flow.component.datetimepicker;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasComponents;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.router.Route;
+
+@Tag("div")
+@Route(value = "date-time", registerAtStartup = false)
+public class DateTimePickerView extends Component implements HasComponents {
+    DateTimePicker picker;
+
+    public DateTimePickerView() {
+        picker = new DateTimePicker();
+        add(picker);
+    }
+}
---
+++ vaadin-testbench-unit/src/test/java/com/vaadin/flow/component/datetimepicker/DateTimePickerWrapTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2022 Vaadin Ltd
+ *
+ * This program is available under Commercial Vaadin Developer License
+ * 4.0 (CVDLv4).
+ *
+ *
+ * For the full License, see <https://vaadin.com/license/cvdl-4.0>.
+ */
+package com.vaadin.flow.component.datetimepicker;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.vaadin.flow.component.AbstractField;
+import com.vaadin.flow.component.HasValue;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.RouteConfiguration;
+import com.vaadin.testbench.unit.UIUnitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateTimePickerWrapTest extends UIUnitTest {
+
+    DateTimePickerView view;
+    DateTimePickerWrap<DateTimePicker> pick_;
+
+    @Override
+    protected String scanPackage() {
+        return getClass().getPackageName();
+    }
+
+    @BeforeEach
+    public void registerView() {
+        RouteConfiguration.forApplicationScope()
+                .setAnnotatedRoute(DateTimePickerView.class);
+        view = navigate(DateTimePickerView.class);
+        pick_ = wrap(view.picker);
+    }
+
+    @Test
+    void invalidValue_overMaxDate_throwsIllegalArgument() {
+        view.picker.setMax(
+                LocalDateTime.of(LocalDate.of(1995, 1, 1), LocalTime.NOON));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> pick_.setValue(LocalDateTime.of(LocalDate.of(1995, 1, 5),
+                        LocalTime.MIDNIGHT)));
+    }
+
+    @Test
+    void invalidValue_underMinDate_throwsIllegalArgument() {
+        view.picker.setMin(
+                LocalDateTime.of(LocalDate.of(1995, 1, 5), LocalTime.NOON));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> pick_.setValue(LocalDateTime.of(LocalDate.of(1995, 1, 1),
+                        LocalTime.MIDNIGHT)));
+    }
+
+    @Test
+    void setValue_eventIsFired_valueIsSet() {
+
+        AtomicReference<LocalDateTime> value = new AtomicReference<>(null);
+
+        view.picker.addValueChangeListener(
+                (HasValue.ValueChangeListener<AbstractField.ComponentValueChangeEvent<DateTimePicker, LocalDateTime>>) event -> {
+                    value.compareAndSet(null, event.getValue());
+                });
+
+        final LocalDateTime newValue = LocalDateTime
+                .of(LocalDate.of(1995, 1, 5), LocalTime.NOON);
+        pick_.setValue(newValue);
+
+        Assertions.assertEquals(newValue, value.get());
+    }
+}
