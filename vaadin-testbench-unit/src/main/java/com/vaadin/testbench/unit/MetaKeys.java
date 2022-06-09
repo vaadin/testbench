07d229b6b045d7df992fb4f738f190a8df7fc9f6
Mikael Grankvist <mikael.grankvist@vaadin.com>
2022-06-09 08:25:06 +0300

    fix: Make MetaKeys setters fluent.
    Make setters fluent for better ux
    as per tester feedback

--- vaadin-testbench-unit/src/main/java/com/vaadin/testbench/unit/MetaKeys.java
+++ vaadin-testbench-unit/src/main/java/com/vaadin/testbench/unit/MetaKeys.java
@@ -18,9 +18,22 @@ public class MetaKeys {
     private boolean alt = false;
     private boolean meta = false;
 
+    /**
+     * Construct metaKeys with all set to false.
+     */
     public MetaKeys() {
     }
 
+    /**
+     * Construct MetaKeys with given values for the keys.
+     *
+     * @param ctrl
+     *            {@code true} for ctrl pressed
+     * @param shift
+     *            {@code true} for shift pressed
+     * @param alt{@code true} for alt pressed
+     * @param meta{@code true} for meta pressed
+     */
     public MetaKeys(boolean ctrl, boolean shift, boolean alt, boolean meta) {
         this.ctrl = ctrl;
         this.shift = shift;
@@ -28,38 +41,125 @@ public class MetaKeys {
         this.meta = meta;
     }
 
+    /**
+     * Set ctrl key down to {@code true}.
+     *
+     * @return this instance
+     */
+    public MetaKeys ctrl() {
+        return setCtrl(true);
+    }
+
+    /**
+     * Set shift key down to {@code true}.
+     *
+     * @return this instance
+     */
+    public MetaKeys shift() {
+        return setShift(true);
+
+    }
+
+    /**
+     * Set alt key down to {@code true}.
+     *
+     * @return this instance
+     */
+    public MetaKeys alt() {
+        return setAlt(true);
+
+    }
+
+    /**
+     * Set meta key down to {@code true}.
+     *
+     * @return this instance
+     */
+    public MetaKeys meta() {
+        return setMeta(true);
+
+    }
+
+    /**
+     * Set ctrl key down state to given value.
+     *
+     * @param ctrl
+     *            key down state
+     * @return this instance
+     */
     public MetaKeys setCtrl(boolean ctrl) {
         this.ctrl = ctrl;
         return this;
     }
 
+    /**
+     * Set shift key down state to given value.
+     *
+     * @param shift
+     *            key down state
+     * @return this instance
+     */
     public MetaKeys setShift(boolean shift) {
         this.shift = shift;
         return this;
     }
 
+    /**
+     * Set alt key down state to given value.
+     *
+     * @param alt
+     *            key down state
+     * @return this instance
+     */
     public MetaKeys setAlt(boolean alt) {
         this.alt = alt;
         return this;
     }
 
+    /**
+     * Set meta key down state to given value.
+     *
+     * @param meta
+     *            key down state
+     * @return this instance
+     */
     public MetaKeys setMeta(boolean meta) {
         this.meta = meta;
         return this;
     }
 
+    /**
+     * Get ctrl key isPressed state.
+     *
+     * @return {@code ture} is pressed
+     */
     public boolean isCtrl() {
         return ctrl;
     }
 
+    /**
+     * Get shift key isPressed state.
+     *
+     * @return {@code ture} is pressed
+     */
     public boolean isShift() {
         return shift;
     }
 
+    /**
+     * Get alt key isPressed state.
+     *
+     * @return {@code ture} is pressed
+     */
     public boolean isAlt() {
         return alt;
     }
 
+    /**
+     * Get meta key isPressed state.
+     *
+     * @return {@code ture} is pressed
+     */
     public boolean isMeta() {
         return meta;
     }
