48eecb4a3e22fc93368cbbc735dbe27c453c73ff
caalador <mikael.grankvist@vaadin.com>
2022-06-15 08:43:23 +0300

    feat: Wrapper for messages (#1469)
    part of #1371

---
+++ vaadin-testbench-unit/src/main/java/com/vaadin/flow/component/messages/MessageInputWrap.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2022 Vaadin Ltd
+ *
+ * This program is available under Commercial Vaadin Developer License
+ * 4.0 (CVDLv4).
+ *
+ *
+ * For the full License, see <https://vaadin.com/license/cvdl-4.0>.
+ */
+package com.vaadin.flow.component.messages;
+
+import com.vaadin.flow.component.ComponentUtil;
+import com.vaadin.testbench.unit.ComponentWrap;
+import com.vaadin.testbench.unit.Wraps;
+
+/**
+ * Test wrapper for MessageInput components.
+ *
+ * @param <T>
+ *            component type
+ */
+@Wraps(MessageInput.class)
+public class MessageInputWrap<T extends MessageInput> extends ComponentWrap<T> {
+    /**
+     * Wrap given component for testing.
+     *
+     * @param component
+     *            target component
+     */
+    public MessageInputWrap(T component) {
+        super(component);
+    }
+
+    /**
+     * Send an input message through the MessageInput component.
+     *
+     * @param message
+     *            message from component
+     */
+    public void send(String message) {
+        ensureComponentIsUsable();
+        if (message == null || message.isEmpty()) {
+            return;
+        }
+        ComponentUtil.fireEvent(getComponent(),
+                new MessageInput.SubmitEvent(getComponent(), true, message));
+    }
+}
