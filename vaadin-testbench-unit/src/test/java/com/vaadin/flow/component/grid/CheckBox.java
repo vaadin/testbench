bdc05802bda460293ee8a39456e6ea70d5587583
caalador <mikael.grankvist@vaadin.com>
2022-06-15 13:35:43 +0300

    feat: add method to get the item component (#1479)
    Add method for getting the component
    for a cell in a grid.
    
    Fixes #1467

---
+++ vaadin-testbench-unit/src/test/java/com/vaadin/flow/component/grid/CheckBox.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2022 Vaadin Ltd
+ *
+ * This program is available under Commercial Vaadin Developer License
+ * 4.0 (CVDLv4).
+ *
+ *
+ * For the full License, see <https://vaadin.com/license/cvdl-4.0>.
+ */
+package com.vaadin.flow.component.grid;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Tag;
+
+@Tag("check-box")
+public class CheckBox extends Component {
+    boolean checked;
+
+    public CheckBox(boolean checked) {
+        this.checked = checked;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+}
