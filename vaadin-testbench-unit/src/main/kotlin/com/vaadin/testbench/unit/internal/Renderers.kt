fe7d1a5353638f1d3a18534858e9aaaaaea4a4e5
Zhe Sun <zhe@vaadin.com>
2022-11-30 13:29:39 +0200

    chore: update vaadin license (8.1)

--- vaadin-testbench-unit/src/main/kotlin/com/vaadin/testbench/unit/internal/Renderers.kt
+++ vaadin-testbench-unit/src/main/kotlin/com/vaadin/testbench/unit/internal/Renderers.kt
@@ -1,5 +1,12 @@
 package com.vaadin.testbench.unit.internal
-
+/**
+ * Copyright (C) 2000-2022 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
+ */
 import com.vaadin.flow.component.Component
 import com.vaadin.flow.data.renderer.BasicRenderer
 import com.vaadin.flow.data.renderer.ComponentRenderer
