90627a9ea4943816cce6bed0d95785bbd97a0840
Artur <artur@vaadin.com>
2023-03-09 09:52:24 +0200

    fix: Make Chrome 111 work (#1611)
    Uses the new JDK 11 based HTTP client which does not have a problem with sending extra Origin-headers
    Related to https://github.com/SeleniumHQ/selenium/issues/11750

--- vaadin-testbench-shared/src/main/java/com/vaadin/testbench/TestBench.java
+++ vaadin-testbench-shared/src/main/java/com/vaadin/testbench/TestBench.java
@@ -86,6 +86,8 @@ public class TestBench {
     static {
         LicenseChecker.checkLicenseFromStaticBlock("vaadin-testbench",
                 TestBenchVersion.testbenchVersion, null);
+        // Enable the Java 11+ HTTP client
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
     }
 
     public static TestBenchDriverProxy createDriver(WebDriver driver) {
