de309ccdcbc09020d83fde79a42907d7c16e3b26
Artur <artur@vaadin.com>
2023-05-12 16:40:08 +0300

    Revert "Ignore test that breaks Saucelabs (#1637)"
    This reverts commit 59cd0c73ae0fd645b228eb4441f38dd68f61dc72.

--- vaadin-testbench-integration-tests/src/test/java/com/vaadin/tests/SVGIT.java
+++ vaadin-testbench-integration-tests/src/test/java/com/vaadin/tests/SVGIT.java
@@ -10,7 +10,6 @@ package com.vaadin.tests;
 
 import com.vaadin.testbench.parallel.BrowserUtil;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
@@ -25,7 +24,6 @@ public class SVGIT extends AbstractTB6Test {
     }
 
     @Test
-    @Ignore("This breaks with 'no session id' on the findElement command since 2023-05-11 because of what seems to be a Saucelabs issue")
     public void click() {
         if (BrowserUtil.isSafari(this.getDesiredCapabilities())) {
             return; // Skip for Safari 11.
