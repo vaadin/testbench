9bebe5c251085e8b50ef9ae5b82a179580272fcc
Marco Collovati <marco@vaadin.com>
2024-02-06 13:43:37 +0100

    fix: prevent fetching virtual childred for Composite
    The Composite class overrides getChildren() to return a stream with the
    encapsulated component, but also getElement() returning the Element of the
    wrapped component. The latter causes the virtual children to be fetched as
    Composite direct children, thus duplicating any virtual children the
    encapsulated component might have.
    
    Fixes #1738
    
    Co-authored-by: Martin Vysny <mavi@vaadin.com>

---
+++ vaadin-testbench-unit-junit5/src/test/kotlin/com/vaadin/testbench/unit/internal/CompositeTest.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2000-2024 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
+ */
+
+package com.vaadin.testbench.unit.internal
+
+import com.vaadin.flow.component.Component
+import com.vaadin.flow.component.Composite
+import com.vaadin.flow.component.Tag
+import com.vaadin.flow.component.html.Span
+import com.github.mvysny.dynatest.DynaNodeGroup
+import com.github.mvysny.dynatest.DynaTestDsl
+
+@DynaTestDsl
+internal fun DynaNodeGroup.compositeTests() {
+    beforeEach { MockVaadin.setup() }
+    afterEach { MockVaadin.tearDown() }
+
+    test("Composite<*> causes virtual children to be fetched twice") {
+        class MyDialog : Composite<VirtualChildComponent>() {
+        }
+
+        val dlg = MyDialog()
+        dlg._expectOne<Span> { text = "virtual child" }
+    }
+}
+
+@Tag("my-test")
+class VirtualChildComponent : Component() {
+    init {
+        val child = Span("virtual child")
+        element.appendVirtualChild(child.element)
+    }
+}
