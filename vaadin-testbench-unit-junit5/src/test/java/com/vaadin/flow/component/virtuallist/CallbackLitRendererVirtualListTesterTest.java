2164a9f7412113dc25e13b5b773cad22ba7df545
Joel Robertson <joel@vaadin.com>
2024-05-21 01:50:06 -0600

    Removed unused import.

--- vaadin-testbench-unit-junit5/src/test/java/com/vaadin/flow/component/virtuallist/CallbackLitRendererVirtualListTesterTest.java
+++ vaadin-testbench-unit-junit5/src/test/java/com/vaadin/flow/component/virtuallist/CallbackLitRendererVirtualListTesterTest.java
@@ -15,8 +15,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 @ViewPackages
 class CallbackLitRendererVirtualListTesterTest extends UIUnitTest {
 
