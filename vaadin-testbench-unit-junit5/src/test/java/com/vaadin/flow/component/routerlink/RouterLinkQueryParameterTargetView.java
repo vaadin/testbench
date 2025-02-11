1768e19be079d051cb3de8a247847057fe7a3f42
Joel Robertson <62086947+joelpop@users.noreply.github.com>
2025-02-11 02:29:22 -0700

    Added tester for RouterLink. (#1810)
    * Added tester for RouterLink.
    
    * refactor tests
    
    ---------
    
    Co-authored-by: Marco Collovati <marco@vaadin.com>
    Co-authored-by: Mikhail Shabarov <61410877+mshabarov@users.noreply.github.com>

---
+++ vaadin-testbench-unit-junit5/src/test/java/com/vaadin/flow/component/routerlink/RouterLinkQueryParameterTargetView.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2000-2025 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
+ */
+package com.vaadin.flow.component.routerlink;
+
+import java.util.stream.Collectors;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.Route;
+
+@Tag(Tag.DIV)
+@Route(value = RouterLinkQueryParameterTargetView.ROUTE, registerAtStartup = false)
+public class RouterLinkQueryParameterTargetView extends AbstractTargetView
+        implements BeforeEnterObserver {
+
+    public static final String ROUTE = "router-link-query-parameter-target";
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        var queryParameters = event.getLocation().getQueryParameters();
+        message.setText(
+                "Query Parameter Target View: { "
+                        + queryParameters.getParameters().entrySet().stream()
+                                .map(entry -> entry.getKey() + " = ["
+                                        + entry.getValue().stream().sorted()
+                                                .collect(Collectors
+                                                        .joining(", "))
+                                        + "]")
+                                .sorted().collect(Collectors.joining("; "))
+                        + " }");
+    }
+}
