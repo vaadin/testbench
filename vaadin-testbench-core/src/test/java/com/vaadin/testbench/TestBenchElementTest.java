cb11522fe595355cb23cd53f172279696a118838
Tatu Lund <tatu@vaadin.com>
2025-03-28 13:38:14 +0200

    Fix test

--- vaadin-testbench-core/src/test/java/com/vaadin/testbench/TestBenchElementTest.java
+++ vaadin-testbench-core/src/test/java/com/vaadin/testbench/TestBenchElementTest.java
@@ -46,7 +46,7 @@ public class TestBenchElementTest {
     public void test_getId_VaadinComponentWithId_returnsId()
             throws Exception {
         WebElement webElement = Mockito.mock(WebElement.class);
-        Mockito.when(webElement.getDomAttribute("id"))
+        Mockito.when(webElement.getAttribute("id"))
                 .thenReturn("identification");
 
         TestBenchElement element = TestBenchElement.wrapElement(webElement,
