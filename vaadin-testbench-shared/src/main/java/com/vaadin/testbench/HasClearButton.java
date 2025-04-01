16001b5ef08753b0931a2f2cbb9548801bde8605
Tatu Lund <tatu@vaadin.com>
2025-04-01 10:58:56 +0300

    feat: Add HasClearButton mixin interface

---
+++ vaadin-testbench-shared/src/main/java/com/vaadin/testbench/HasClearButton.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2000-2025 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
+ */
+package com.vaadin.testbench;
+
+import com.vaadin.flow.component.html.testbench.DivElement;
+
+/**
+ * The {@code HasClearButton} interface provides methods to interact with an element
+ * that may have a clear button. It includes functionality to check the visibility
+ * of the clear button and to click it if it is visible.
+ */
+public interface HasClearButton extends HasElementQuery, HasCallFunction {
+
+    /**
+     * Checks if the current element has a clear button.
+     *
+     * @return {@code true} if the element has a clear button, {@code false}
+     *         otherwise.
+     */
+    default boolean isClearButtonVisible() {
+        return (boolean) callFunction("hasAttribute", "clear-button-visible");
+    }
+
+    /**
+     * Clicks the clear button if it is visible.
+     * <p>
+     * This method checks if the clear button is visible before attempting to
+     * click it. If the clear button is not visible, an
+     * {@link IllegalStateException} is thrown.
+     * </p>
+     *
+     * @throws IllegalStateException
+     *             if the clear button is not visible
+     */
+    default void clickClearButton() {
+        if (!isClearButtonVisible()) {
+            throw new IllegalStateException("Clear button is not visible");
+        }
+        this.$(DivElement.class).id("clearButton").click();
+    }
+}
