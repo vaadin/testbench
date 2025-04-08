27a3232684d86604b48ab1b6c400d5bb3272c8f4
Tatu Lund <tatu@vaadin.com>
2025-04-08 13:26:38 +0300

    fix: Focus on click (#1906)

--- vaadin-testbench-integration-tests-junit5/src/test/java/com/vaadin/tests/elements/BasicElementIT.java
+++ vaadin-testbench-integration-tests-junit5/src/test/java/com/vaadin/tests/elements/BasicElementIT.java
@@ -32,6 +32,14 @@ public class BasicElementIT extends AbstractBrowserTB9Test {
     }
 
     @BrowserTest
+    public void clickingButtonWillFocusIt() {
+        TestBenchElement buttonElement = $(NativeButtonElement.class)
+                .waitForFirst();
+        buttonElement.click();
+        Assertions.assertTrue(buttonElement.isFocused());
+    }
+
+    @BrowserTest
     public void getChildrenTest() {
         TestBenchElement element = $(TestBenchElement.class).id("element-query-view");
         Assertions.assertEquals(10, element.getChildren().size());
