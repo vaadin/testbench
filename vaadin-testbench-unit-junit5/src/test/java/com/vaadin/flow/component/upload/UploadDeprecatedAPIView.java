2deab9d9fe7a4ee0db636bb1822a47b04077f5c2
Marco Collovati <marco@vaadin.com>
2025-07-30 12:22:47 +0200

    fix: add support for UploadHandler in UploadTester (#1971)

--- vaadin-testbench-unit-junit5/src/test/java/com/vaadin/flow/component/upload/UploadDeprecatedAPIView.java
+++ vaadin-testbench-unit-junit5/src/test/java/com/vaadin/flow/component/upload/UploadDeprecatedAPIView.java
@@ -11,20 +11,25 @@ package com.vaadin.flow.component.upload;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasComponents;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
+import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 import com.vaadin.flow.router.Route;
 
 @Tag("div")
 @Route(value = "upload", registerAtStartup = false)
-public class UploadView extends Component implements HasComponents {
+@Deprecated
+public class UploadDeprecatedAPIView extends Component
+        implements HasComponents {
 
-    final Upload uploadSingle;
-    final Upload uploadMulti;
+    MemoryBuffer receiver = new MemoryBuffer();
+    MultiFileMemoryBuffer multiReceiver = new MultiFileMemoryBuffer();
+    Upload uploadSingle;
+    Upload uploadMulti;
 
-    public UploadView() {
-        uploadSingle = new Upload();
-        uploadMulti = new Upload();
+    public UploadDeprecatedAPIView() {
+        uploadSingle = new Upload(receiver);
+        uploadMulti = new Upload(multiReceiver);
 
         add(uploadSingle, uploadMulti);
     }
-
 }
--- vaadin-testbench-unit-junit5/src/test/java/com/vaadin/flow/component/upload/UploadTesterTest.java
+++ vaadin-testbench-unit-junit5/src/test/java/com/vaadin/flow/component/upload/UploadTesterTest.java
@@ -10,15 +10,16 @@ package com.vaadin.flow.component.upload;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -26,7 +27,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import com.vaadin.flow.component.upload.AssertingTransferProgressListener.UploadedData;
 import com.vaadin.flow.router.RouteConfiguration;
+import com.vaadin.flow.server.streams.UploadHandler;
 import com.vaadin.testbench.unit.UIUnitTest;
 import com.vaadin.testbench.unit.ViewPackages;
 
@@ -79,136 +82,46 @@ class UploadTesterTest extends UIUnitTest {
 
     @Test
     void upload_singleFile_succeeds() {
-
-        AtomicBoolean started = new AtomicBoolean();
-        AtomicBoolean finished = new AtomicBoolean();
         AtomicBoolean allFinished = new AtomicBoolean();
-        AtomicBoolean succeeded = new AtomicBoolean();
-        AtomicBoolean failed = new AtomicBoolean();
 
-        view.uploadSingle.addStartedListener(ev -> {
-            Assertions.assertEquals(ev.getFileName(), file1.getName());
-            Assertions.assertFalse(finished.get(),
-                    "Finished should not have been notified on start");
-            Assertions.assertFalse(allFinished.get(),
-                    "All Finished should not have been notified on start");
-            Assertions.assertFalse(succeeded.get(),
-                    "Succeeded should not have been notified on start");
-            started.set(true);
-        });
-        view.uploadSingle.addSucceededListener(ev -> {
-            Assertions.assertTrue(started.get(),
-                    "Started should have been notified on succeeded");
-            Assertions.assertFalse(finished.get(),
-                    "Finished should not have been notified on start");
-            Assertions.assertFalse(allFinished.get(),
-                    "All Finished should not have been notified on start");
-            succeeded.set(true);
-        });
-        view.uploadSingle.addFinishedListener(ev -> {
-            Assertions.assertTrue(started.get(),
-                    "Started should have been notified on succeeded");
-            Assertions.assertTrue(succeeded.get(),
-                    "Succeeded should have been notified on succeeded");
-            Assertions.assertFalse(allFinished.get(),
-                    "All Finished should not have been notified on start");
-            finished.set(true);
-        });
-        view.uploadSingle.addAllFinishedListener(ev -> {
-            Assertions.assertTrue(started.get(),
-                    "Started should have been notified on succeeded");
-            Assertions.assertTrue(succeeded.get(),
-                    "Succeeded should have been notified on succeeded");
-            Assertions.assertTrue(finished.get(),
-                    "Finished should have been notified on succeeded");
-            allFinished.set(true);
-        });
-        view.uploadSingle.addFailedListener(ev -> Assertions
-                .fail("Failed listener should not be notified"));
+        AssertingTransferProgressListener listener = new AssertingTransferProgressListener();
+        view.uploadSingle.setUploadHandler(
+                UploadHandler.inMemory(listener::fileUploaded, listener));
+        view.uploadSingle.addAllFinishedListener(ev -> allFinished.set(true));
 
         single_.upload(file1);
 
-        Assertions.assertTrue(started.get(),
-                "Started listener was not notified");
-        Assertions.assertTrue(succeeded.get(),
-                "Succeeded listener was not notified");
-        Assertions.assertFalse(failed.get(), "Failed listener was notified");
-        Assertions.assertTrue(finished.get(),
-                "Finished listener was not notified");
+        listener.assertStarted();
+        listener.assertProgressCalled();
+        listener.assertCompleted();
+        listener.assertNotFailed();
         Assertions.assertTrue(allFinished.get(),
                 "All Finished listener was not notified");
 
-        Assertions.assertEquals(file1.getName(), view.receiver.getFileName());
+        UploadedData uploadedData = listener.assertFileReceived();
+        Assertions.assertNotNull(uploadedData);
+        Assertions.assertEquals(file1.getName(),
+                uploadedData.metadata().fileName());
         Assertions.assertEquals("text/plain",
-                view.receiver.getFileData().getMimeType());
+                uploadedData.metadata().contentType());
         Assertions.assertEquals(FIRST_FILE_CONTENTS,
-                inputStreamToString(view.receiver.getInputStream()));
+                uploadedDataToString(uploadedData));
     }
 
     @Test
     void upload_singleFile_failure() {
 
-        AtomicBoolean started = new AtomicBoolean();
-        AtomicBoolean finished = new AtomicBoolean();
         AtomicBoolean allFinished = new AtomicBoolean();
-        AtomicBoolean succeeded = new AtomicBoolean();
-        AtomicBoolean failed = new AtomicBoolean();
-
-        view.uploadSingle.setReceiver((fileName, mimeType) -> {
-            throw new UncheckedIOException(new IOException("BOOM!"));
-        });
-        view.uploadSingle.addStartedListener(ev -> {
-            Assertions.assertEquals(ev.getFileName(), file1.getName());
-            Assertions.assertFalse(finished.get(),
-                    "Finished should not have been notified on start");
-            Assertions.assertFalse(allFinished.get(),
-                    "All Finished should not have been notified on start");
-            Assertions.assertFalse(succeeded.get(),
-                    "Succeeded should not have been notified on start");
-            Assertions.assertFalse(failed.get(),
-                    "Succeeded should not have been notified on start");
-            started.set(true);
-        });
-        view.uploadSingle.addFailedListener(ev -> {
-            Assertions.assertTrue(started.get(),
-                    "Started should have been notified on succeeded");
-            Assertions.assertFalse(finished.get(),
-                    "Finished should not have been notified on start");
-            Assertions.assertFalse(allFinished.get(),
-                    "All Finished should not have been notified on start");
-            failed.set(true);
-        });
-        view.uploadSingle.addFinishedListener(ev -> {
-            Assertions.assertTrue(started.get(),
-                    "Started should have been notified on succeeded");
-            Assertions.assertTrue(failed.get(),
-                    "Failed should have been notified on succeeded");
-            Assertions.assertFalse(allFinished.get(),
-                    "All Finished should not have been notified on start");
-            finished.set(true);
-        });
-        view.uploadSingle.addAllFinishedListener(ev -> {
-            Assertions.assertTrue(started.get(),
-                    "Started should have been notified on succeeded");
-            Assertions.assertTrue(failed.get(),
-                    "Failed should have been notified on succeeded");
-            Assertions.assertTrue(finished.get(),
-                    "Finished should have been notified on succeeded");
-            allFinished.set(true);
-        });
-        view.uploadSingle.addSucceededListener(ev -> Assertions
-                .fail("Succeeded listener should not be notified"));
+        AssertingTransferProgressListener listener = new AssertingTransferProgressListener();
+        view.uploadSingle.setUploadHandler(listener.asFailingHandler());
+        view.uploadSingle.addAllFinishedListener(ev -> allFinished.set(true));
 
         Assertions.assertThrows(UncheckedIOException.class,
                 () -> single_.upload(file1));
 
-        Assertions.assertTrue(started.get(),
-                "Started listener was not notified");
-        Assertions.assertFalse(succeeded.get(),
-                "Succeeded listener was notified");
-        Assertions.assertTrue(failed.get(), "Failed listener was not notified");
-        Assertions.assertTrue(finished.get(),
-                "Finished listener was not notified");
+        listener.assertStarted();
+        listener.assertFailed();
+        listener.assertNotCompleted();
         Assertions.assertTrue(allFinished.get(),
                 "All Finished listener was not notified");
 
@@ -216,52 +129,35 @@ class UploadTesterTest extends UIUnitTest {
 
     @Test
     void upload_multipleFiles_succeeds() {
-        AtomicInteger started = new AtomicInteger();
-        AtomicInteger finished = new AtomicInteger();
-        AtomicInteger allFinished = new AtomicInteger();
-        AtomicInteger succeeded = new AtomicInteger();
-        AtomicInteger failed = new AtomicInteger();
 
-        view.uploadMulti.addStartedListener(ev -> started.incrementAndGet());
-        view.uploadMulti.addFailedListener(ev -> failed.incrementAndGet());
-        view.uploadMulti.addFinishedListener(ev -> finished.incrementAndGet());
+        AtomicInteger allFinished = new AtomicInteger();
+        AssertingTransferProgressListener listener = new AssertingTransferProgressListener();
+        view.uploadMulti.setUploadHandler(
+                UploadHandler.inMemory(listener::fileUploaded, listener));
         view.uploadMulti
                 .addAllFinishedListener(ev -> allFinished.incrementAndGet());
-        view.uploadMulti
-                .addSucceededListener(ev -> succeeded.incrementAndGet());
 
         multi_.uploadAll(file1, file2, file3);
 
-        Assertions.assertEquals(3, started.get(),
-                "Expected Started to be invoked once per uploaded file, but was called "
-                        + succeeded.get() + " times");
-        Assertions.assertEquals(3, succeeded.get(),
-                "Expected Succeeded to be invoked once per uploaded file, but was called "
-                        + succeeded.get() + " times");
-        Assertions.assertEquals(3, finished.get(),
-                "Expected Finished to be invoked once per uploaded file, but was called "
-                        + finished.get() + " times");
-        Assertions.assertEquals(0, failed.get(),
-                "Failed should not be invoked");
+        listener.assertStarted(3);
+        listener.assertCompleted(3);
+        listener.assertNotFailed();
         Assertions.assertEquals(1, allFinished.get(),
                 "All Finished should be invoked once");
 
-        Set<String> receivedFiles = view.multiReceiver.getFiles();
-        Assertions.assertEquals(3, receivedFiles.size());
-        Assertions.assertTrue(receivedFiles.containsAll(
-                Set.of(file1.getName(), file2.getName(), file3.getName())));
+        List<UploadedData> receivedFiles = listener.assertFilesReceived(3);
+        Set<String> fileNames = receivedFiles.stream()
+                .map(ud -> ud.metadata().fileName())
+                .collect(Collectors.toSet());
+        Assertions.assertEquals(
+                Set.of(file1.getName(), file2.getName(), file3.getName()),
+                fileNames);
     }
 
     @Test
     void uploadAll_noFiles_throws() {
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> multi_.uploadAll());
-    }
-
-    @Test
-    void uploadAll_singleReceiver_throws() {
-        Assertions.assertThrows(IllegalStateException.class,
-                () -> single_.uploadAll(file1, file2, file3));
     }
 
     @Test
@@ -282,73 +178,24 @@ class UploadTesterTest extends UIUnitTest {
     }
 
     void assertFailedUpload(BiConsumer<String, String> wrapperAction) {
-        AtomicBoolean started = new AtomicBoolean();
-        AtomicBoolean finished = new AtomicBoolean();
-        AtomicBoolean allFinished = new AtomicBoolean();
-        AtomicBoolean succeeded = new AtomicBoolean();
-        AtomicBoolean failed = new AtomicBoolean();
 
-        view.uploadSingle.addStartedListener(ev -> {
-            Assertions.assertEquals(ev.getFileName(), file1.getName());
-            Assertions.assertFalse(finished.get(),
-                    "Finished should not have been notified on start");
-            Assertions.assertFalse(allFinished.get(),
-                    "All Finished should not have been notified on start");
-            Assertions.assertFalse(succeeded.get(),
-                    "Succeeded should not have been notified on start");
-            Assertions.assertFalse(failed.get(),
-                    "Succeeded should not have been notified on start");
-            started.set(true);
-        });
-        view.uploadSingle.addFailedListener(ev -> {
-            Assertions.assertTrue(started.get(),
-                    "Started should have been notified on succeeded");
-            Assertions.assertFalse(finished.get(),
-                    "Finished should not have been notified on start");
-            Assertions.assertFalse(allFinished.get(),
-                    "All Finished should not have been notified on start");
-            failed.set(true);
-        });
-        view.uploadSingle.addFinishedListener(ev -> {
-            Assertions.assertTrue(started.get(),
-                    "Started should have been notified on succeeded");
-            Assertions.assertTrue(failed.get(),
-                    "Failed should have been notified on succeeded");
-            Assertions.assertFalse(allFinished.get(),
-                    "All Finished should not have been notified on start");
-            finished.set(true);
-        });
-        view.uploadSingle.addAllFinishedListener(ev -> {
-            Assertions.assertTrue(started.get(),
-                    "Started should have been notified on succeeded");
-            Assertions.assertTrue(failed.get(),
-                    "Failed should have been notified on succeeded");
-            Assertions.assertTrue(finished.get(),
-                    "Finished should have been notified on succeeded");
-            allFinished.set(true);
-        });
-        view.uploadSingle.addSucceededListener(ev -> Assertions
-                .fail("Succeeded listener should not be notified"));
+        AtomicBoolean allFinished = new AtomicBoolean();
+        AssertingTransferProgressListener listener = new AssertingTransferProgressListener();
+        view.uploadSingle.setUploadHandler(
+                UploadHandler.inMemory(listener::fileUploaded, listener));
+
+        view.uploadSingle.addAllFinishedListener(ev -> allFinished.set(true));
 
         wrapperAction.accept(file1.getName(), "text/plain");
 
-        Assertions.assertTrue(started.get(),
-                "Started listener was not notified");
-        Assertions.assertFalse(succeeded.get(),
-                "Succeeded listener was notified");
-        Assertions.assertTrue(failed.get(), "Failed listener was not notified");
-        Assertions.assertTrue(finished.get(),
-                "Finished listener was not notified");
+        listener.assertStarted();
+        listener.assertNotCompleted();
+        listener.assertFailed();
         Assertions.assertTrue(allFinished.get(),
                 "All Finished listener was not notified");
     }
 
-    private String inputStreamToString(InputStream inputStream) {
-        try {
-            return new String(inputStream.readAllBytes(),
-                    StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
+    private String uploadedDataToString(UploadedData uploadedData) {
+        return new String(uploadedData.data(), StandardCharsets.UTF_8);
     }
 }
