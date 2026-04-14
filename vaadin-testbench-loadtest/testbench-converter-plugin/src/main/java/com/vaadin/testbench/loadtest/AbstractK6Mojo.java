6c03c389b2d3be11f6d44ee8f98e2576bd410610
caalador <mikael.grankvist@vaadin.com>
2026-04-14 15:07:17 +0300

    chore: log experimental warning (#2192)
    Add log that loadtest is
    experimental.

--- vaadin-testbench-loadtest/testbench-converter-plugin/src/main/java/com/vaadin/testbench/loadtest/AbstractK6Mojo.java
+++ vaadin-testbench-loadtest/testbench-converter-plugin/src/main/java/com/vaadin/testbench/loadtest/AbstractK6Mojo.java
@@ -21,6 +21,7 @@ import org.apache.maven.project.MavenProject;
 import com.vaadin.pro.licensechecker.Capabilities;
 import com.vaadin.pro.licensechecker.Capability;
 import com.vaadin.pro.licensechecker.LicenseChecker;
+import com.vaadin.testbench.loadtest.util.ExperimentalWarning;
 import com.vaadin.testbench.loadtest.util.NodeRunner;
 import com.vaadin.testbench.loadtest.util.ResourceExtractor;
 import com.vaadin.testbench.loadtest.util.ResponseCheckConfig;
@@ -111,6 +112,8 @@ public abstract class AbstractK6Mojo extends AbstractMojo {
      */
     protected void initialize() throws MojoExecutionException {
         checkLicense();
+
+        ExperimentalWarning.log();
 
         extractionPath = Path.of(utilsDir);
 
